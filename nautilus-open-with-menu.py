5cd012af59bd789e7b78f01be47f7590b6228c81
Lord Yuuma <lordyuuma@gmail.com>
2016-04-18 23:16:32 +0200

    Implement MIME based reduction of suggested apps
    Earlier versions would suggest all applications that could be used to open any file in our list.
    With this commit only the applications opening all files are shown

--- nautilus-open-with-menu.py
+++ nautilus-open-with-menu.py
@@ -7,7 +7,7 @@
 # Created: Mon Apr 18 17:50:46 2016 (+0200)
 # Version: 
 # Package-Requires: ()
-# Last-Updated: Mon Apr 18 22:36:37 2016 (+0200)
+# Last-Updated: Mon Apr 18 23:09:35 2016 (+0200)
 #           By: Lord Yuuma
 # URL: 
 # Doc URL: 
@@ -48,24 +48,41 @@ import gi
 gi.require_version('Nautilus', '3.0')
 from gi.repository import Nautilus, GObject, Gio
 
+from functools import reduce
+from operator import and_
+
+
 class NautilusOpenWithMenu(Nautilus.MenuProvider, GObject.GObject):
 
     def __init__(self):
         pass
     
     def get_file_items(self, window, files):
+        if not len(files):
+            return None
+        
         root_item = Nautilus.MenuItem(name = "NautilusOpenWithMenu::root",
                                       label = "Open With...")
         sub_menu = Nautilus.Menu()
         root_item.set_submenu(sub_menu)
 
-        apps = []
-        for file in files:
-            mime_type = file.get_mime_type()
-            for app_info in Gio.app_info_get_all_for_type(mime_type):
-                apps.append(app_info)
+        # for a large number of files, this is a good optimization, especially when they all have the same mimetype
+        # consider for instance opening thousand of music files with VLC or something similar and having to check all applications for each file
+        mimetypes = list(set([file.get_mime_type() for file in files]))
+        
+        # here we search for ApplicationInfos, that are common for all
+        # because we want them to open ALL the files, they have to be a subset of the applications that can open the first
+        apps = [app_info for app_info in Gio.app_info_get_all_for_type(mimetypes[0])]
 
-        if len(apps) < 2:
+        # here we build the list of name of applications that can be used for all files 
+        names = [set([app_info.get_name() for app_info in Gio.app_info_get_all_for_type(type)])
+                 for type in mimetypes]
+        names = list(reduce(and_, names))
+
+        # and then we reduce our application list to what is left
+        apps = [app for app in apps if app.get_name() in names]
+
+        if len(apps) < 2 and len(mimetypes) == 1:
             return []
 
         for app in apps:
